1f4d2495e11e43a953dd47303f8c38ee10bb10cf
devrenjith singh <devrenith_singh@epam.com>
2025-04-08 17:36:05 +0530

    Add project files.

---
+++ Task1/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Product other)
+            {
+                return Name == other.Name && Price == other.Price;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Price);
+        }
+    }
+}
